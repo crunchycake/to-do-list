a071703cacb2d2906c07aa8336ac3f49a8fae626
Mateusz Cieślik <mateuszcieslik@gmail.com>
2022-02-22 18:49:14 +0100

    added newTodo function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let todoInput
 let errorInfo
 let addBtn
 let ulList
+let newTodo //nowe zadanie
 
 const main = () => {
     // wywoływanie poniższych funkcji
@@ -19,6 +20,33 @@ const prepareDOMEelements = () => {
 
 const prepareDOMEevents = () => {
     // nasłuchiwanie
+    addBtn.addEventListener('click', addNewTodo)
 }
+
+const addNewTodo = () => {
+    if(todoInput.value !==''){
+        console.log('ok')
+        newTodo = document.createElement('li')
+        newTodo.textContent = todoInput.value
+        console.log(newTodo)
+        ulList.append(newTodo)
+        todoInput.value = ''
+        errorInfo.textContent = ''
+    } else {
+        errorInfo.textContent = 'Wpisz treść zadania'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', main)
