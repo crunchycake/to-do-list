fbf7b46909e13f8ac098df18e4d4eda55141ddb8
Mateusz Cieślik <mateuszcieslik@gmail.com>
2022-02-23 18:50:58 +0100

    added popup

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ let errorInfo
 let addBtn
 let ulList
 let newTodo //nowe zadanie
+let popup 
+let popupInfo //tekst w popupie, jak się nie doda pusty tekst
+let todoEdit //edytowany Todo
+let popupInput //input w popupie
+let popupAddBtn //przycisk zatwierdź w popupie
+let popupCloseBtn // przycisk anuluj w popupie
 
 const main = () => {
 	// wywoływanie poniższych funkcji
@@ -16,11 +22,19 @@ const prepareDOMEelements = () => {
 	errorInfo = document.querySelector('.error-info')
 	addBtn = document.querySelector('.btn-add')
 	ulList = document.querySelector('.todolist ul')
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
 }
 
 const prepareDOMEevents = () => {
 	// nasłuchiwanie
 	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkClick)
+	popupCloseBtn.addEventListener('click', closePopup)
+
 }
 
 // 1. tworzymy nowy element (li)
@@ -63,6 +77,42 @@ const createToolsArea = () => {
 
 	toolsPanel.append(completeBtn, editBtn, deleteBtn)
 }
+
+const checkClick = (e) => {
+	if(e.target.matches('.complete')) {
+
+e.target.closest('li').classList.toggle('completed');
+e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')){
+		editTodo()
+	} else if (e.target.matches('.delete')){
+		console.log('delete');
+	}
+
+	
+}
+
+
+
+const editTodo = () => {
+	popup.style.display = 'flex'
+
+}
+
+
+const closePopup = () => {
+	popup.style.display = 'none'
+}
+
+
+
+
+
+
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', main)
 
